951cdbad9185e2b352fa13060f4a3d5e99677ac5
Ayaaz Street <ayaazstreet@gmail.com>
2022-10-26 12:45:58 +0200

    Singleton structure (need to add the creation of other classes)
    Co-authored-by: TheMythik <mark@dreamwings.co.za>

---
+++ code/WarEngine/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "WarEngine.h"
+
+using namespace std;
+
+int main(){
+  /*
+  WarEngine::instance().printWarEngineData();
+  WarEngine::instance().updateWarEngineData();
+  WarEngine::instance().printWarEngineData();
+  */
+}
