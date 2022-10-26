fb20d92c5f43ad866a14fc6a64369d1de8cf6c56
KiaraJaimungal <70319441+KiaraJaimungal@users.noreply.github.com>
2022-10-26 08:24:42 +0200

    Add files via upload

--- code/Resources/main.cpp
+++ code/Resources/main.cpp
@@ -5,6 +5,13 @@
 #include "LandVehicle.cpp"
 #include "Tank.cpp"
 #include "CargoTruck.cpp"
+#include "AirVehicle.cpp"
+#include "Fighterjet.cpp"
+#include "CargoPlane.cpp"
+#include "SeaVehicle.cpp"
+#include "Submarine.cpp"
+#include "CargoShip.cpp"
+
 
 
 
@@ -16,6 +23,7 @@ int main()
 
 	//This main tests the resources structure aka the Abstract Factory Pattern
 
+	//LAND
 
 	//Testing the creation of an army land vehicle aka tank
 	Vehicle *army1 = new ArmyVehicle();
@@ -35,7 +43,49 @@ int main()
 	delete myCargoTruck;
 	delete army2;
 
+	cout<< endl;
 
+	///////////////////////////////////////////////////////
+	//AIR
+	//Testing the creation of an air vehicle aka Fighterjet
+	Vehicle *army3 = new ArmyVehicle();
 
+	AirVehicle* myFighterjet = army3->createAirVehicle();
 
+	delete myFighterjet;
+	delete army3;
+
+	cout<< endl;
+
+	//Testing the creation of a air vehicle aka CargoPLane
+	Vehicle *army4 = new TransportVehicle();
+
+	AirVehicle* myCargoPlane= army4->createAirVehicle();
+
+	delete myCargoPlane;
+	delete army4;
+
+	cout<< endl;
+
+	///////////////////////////////////////////////////////
+	//SEA
+	//Testing the creation of an Sea vehicle aka Submarine
+	Vehicle *army5 = new ArmyVehicle();
+
+	SeaVehicle* mySubmarine = army5->createSeaVehicle();
+
+	delete mySubmarine;
+	delete army5;
+
+	cout<< endl;
+
+	//Testing the creation of a Sea vehicle aka CargoShip
+	Vehicle *army6 = new TransportVehicle();
+
+	SeaVehicle* myCargoShip= army6->createSeaVehicle();
+
+	delete myCargoShip;
+	delete army6;
+
+	cout<< endl;
 }
