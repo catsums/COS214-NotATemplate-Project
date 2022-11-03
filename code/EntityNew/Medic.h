bfcb9a411bd9a0ecb9a4f8dd11d81ae971d1dd79
KiaraJaimungal <kiarajaimungal2039@gmail.com>
2022-11-03 10:11:01 +0200

    Doxygen

--- code/EntityNew/Medic.h
+++ code/EntityNew/Medic.h
@@ -1,3 +1,11 @@
+/** @file Medic.h
+ *  @brief This is the header file to create the medic class.
+ *
+ *  Creating an entity class allows us to create our various elements that are invloved in the war
+ *  an example of an entity is a Citizen after creating a citizen we must establish the different 
+ *	types of citizens and their role in the war. This is one of them. 
+ */
+
 #ifndef MEDIC_H
 #define MEDIC_H
 
@@ -11,26 +19,43 @@ class Medic: public Citizen
 
 private:
 
-	int healAmount;
+	int healAmount; /**int variable representing the heal amount the medic has*/
 
 
 public: 
 
-	Medic();// default constuctor 
-	Medic(int,string,int,int,int);// param constuctor 
-	
-	virtual ~Medic(); //destructor
 
-	//getters
+ 	/** Default constructor for informant
+    */
+	Medic(); 
+
+	/** A parameter constructer taht initializes 5 parameters inherits the variables from the citizen class  
+	*
+	*	@param int variable that represents the health
+	*	@param string variable that represents the country
+	*	@param int variable that represents the damage
+	*	@param int variable that represents the x-coordinates
+	*	@param int variable that represents the y-coordinates 
+    */
+	Medic(int,string,int,int,int);
 	
-	/*
-	This function will provide health to other entities.
-	It returns an integer value that will be added to an entity's health
+
+	/** Destructor
+    */
+	virtual ~Medic(); 
+
+	//Getters
+	
+	/** This function will provide health to other entities.
+	*	It returns an integer value that will be added to an entity's health
 	*/
 	int getHeal();
 
-	//setters
+	///Setters
 
+	/** This function will set the health to other entities.
+	*	@param int variable that sets the health the medic has to offer 
+	*/
 	void setHeal(int h);
 
 
--- code/EntityNew/Politician.h
+++ code/EntityNew/Politician.h
@@ -1,3 +1,13 @@
+/** @file Politician.h
+ *  @brief This is the header file to create the politician class.
+ *
+ *  Creating an entity class allows us to create our various elements that are invloved in the war
+ *  an example of an entity is a Citizen after creating a citizen we must establish the different 
+ *	types of citizens and their role in the war. This is one of them. 
+ */
+
+
+
 #ifndef POLITICIAN_H
 #define POLITICIAN_H
 
@@ -12,10 +22,25 @@ class Politician: public Citizen
 
 public: 
 
-	Politician();// default constuctor 
-	Politician(int,string,int,int,int);// param constuctor 
+	/** Default constructor for informant
+    */
+	Politician();
+
+
+	/** A parameter constructer taht initializes 5 parameters inherits the variables from the citizen class  
+	*
+	*	@param int variable that represents the health
+	*	@param string variable that represents the country
+	*	@param int variable that represents the damage
+	*	@param int variable that represents the x-coordinates
+	*	@param int variable that represents the y-coordinates 
+    */
+	Politician(int,string,int,int,int); 
 	
-	virtual ~Politician(); //destructor
+
+	/** Destructor
+    */
+	virtual ~Politician(); 
 
 	
 
