8d60b6eeb887d4f9e622e775755f74d41b049b82
TheMythik <mark@dreamwings.co.za>
2022-11-03 17:53:26 +0200

    Update Battle.cpp
    Fixed some minor issues
    
    Co-Authored-By: Ayaaz Street <90754133+AyaazStreet@users.noreply.github.com>

--- code/Battle/Battle.cpp
+++ code/Battle/Battle.cpp
@@ -1,7 +1,9 @@
 #include "Battle.h"
 
-Battle::Battle(Country* c1, Country* c2, int t) : c1(c1), c1(c2), theatre(t) {}
+#include <iostream>
+
+Battle::Battle(Country* country1, Country* country2, int t) : c1(country1), c2(country2), theatre(t) {}
 
 void Battle::commence() {
-    cout << "Battle commenced " << c1->getName << " VS " << c2->getName << endl;
+    cout << "Battle commenced " << c1->getName() << " VS " << c2->getName() << endl;
 }
