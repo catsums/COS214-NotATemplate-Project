b205694cada99fafda9358e242d96cbe56f51eaa
KiaraJaimungal <kiarajaimungal2039@gmail.com>
2022-11-07 07:54:30 +0200

    Doxygen

--- code/SignalHandler/ObjectSignalEvent.h
+++ code/SignalHandler/ObjectSignalEvent.h
@@ -1,3 +1,8 @@
+/** @file ObjectSignalEvent.h
+*	@brief ObjectSignalEvent is an event that contains data of a specific type which can be passed on in a handler
+*  
+*/
+
 #ifndef OBJECTSIGNALEVENT_H
 #define OBJECTSIGNALEVENT_H
 
@@ -14,24 +19,28 @@
 
 using namespace std;
 
-/*	
-	ObjectSignalEvent is an event that contains data of a specific type which can be passed on in a handler
-*/
 
 template<class T>
 class ObjectSignalEvent: public SignalEvent{
 public:
-	//create signalevent with the corresponding data
+	/** create signalevent with the corresponding data
+	*	@param string name 
+	*	@param data
+	*/
 	ObjectSignalEvent(string n, T* data);
+
+	/** Parameter constructor
+	*	@para bject signal even refrence 
+	*/
 	ObjectSignalEvent(ObjectSignalEvent& other);
-	~ObjectSignalEvent();
-	//create clone of signal event
-	SignalEvent* clone();
-	//get the data passed on in the event
-	T* getData() const;
+	~ObjectSignalEvent();///Destructor
+	
+	SignalEvent* clone(); /**<create clone of signal event*/
+	
+	T* getData() const; /**<get the data passed on in the event*/
 public:
-	//data of the event as a pointer
-	T* data;
+	
+	T* data; /**<data of the event as a pointer*/
 };
 
 
--- code/SignalHandler/ObjectSignalHandler.h
+++ code/SignalHandler/ObjectSignalHandler.h
@@ -1,3 +1,8 @@
+/** @file ObjectSignalHandler.h
+ *  
+ */
+
+
 #ifndef OBJECTSIGNALHANDLER_H
 #define OBJECTSIGNALHANDLER_H
 
@@ -17,29 +22,54 @@ using namespace std;
 template<class T>
 class ObjectSignalEvent: public SignalEvent{
 public:
+	/** Parameter constructor
+	*	@param string name
+	*	@param data 
+	*/
 	ObjectSignalEvent(string n, T* data);
+
+	/** Parameter constructor
+	*	@param Reference of a object signal event
+	*/
 	ObjectSignalEvent(ObjectSignalEvent& other);
+
+	///Destructor
 	~ObjectSignalEvent();
 
-	SignalEvent* clone();
+	SignalEvent* clone(); /**<Signal event pointer which clones*/
 
-	T* getData() const;
+	T* getData() const; /**<retrieves data*/
 public:
-	T* data;
+	T* data; /**<holds data*/
 };
 
 template<class T>
 class ObjectSignalBus: public SignalBus{
 public:
-	ObjectSignalBus();
-	~ObjectSignalBus();
+	ObjectSignalBus(); /**<Constructor*/
+	~ObjectSignalBus(); /**<Destructor*/
 
-	virtual void emit(string n);
-	virtual void emit(string n, T* _data);
+	/**
+	*	@param string name 
+	*/
+	virtual void emit(string n); /**<emit function*/
+
+	/**
+	*	@param string name 
+	*	@param data 
+	*/
+	virtual void emit(string n, T* _data); /**<emit function*/
 
 protected:
-	virtual SignalEvent* createSignalEvent(string n);
-	virtual SignalEvent* createSignalEvent(string n, T* _data);
+	/**
+	*	@param string name 
+	*/
+	virtual SignalEvent* createSignalEvent(string n); /**<signal event*/
+	/**
+	*	@param string name 
+	*	@param data 
+	*/
+	virtual SignalEvent* createSignalEvent(string n, T* _data); /**<signal event with data*/
 };
 
 #endif
